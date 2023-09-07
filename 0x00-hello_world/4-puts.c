6c4329641e4f5b7c90adc65cd358cd2f320bf2cf
kelvinehiz <kelvinehiz@gmail.com>
2023-09-07 15:11:54 -0700

    A shell program that looks at some basic command in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
 
-
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
 {
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,12 @@
-#!/bin/bash
 #include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
 {
