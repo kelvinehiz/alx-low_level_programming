4ff25598ece196f450d65d68ac254f8b1e7d2721
kelvinehiz <kelvinehiz@gmail.com>
2023-09-09 01:40:56 -0700

    Changed the size of 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,22 +12,17 @@ int main(void)
 {
 
         /* sizeof evaluates the size of a variable */
+        printf("Size of a char: %zu byte(s)\n", sizeof(char));
+        printf("Size of an int: %zu byte(s)\n", sizeof(int));
+        printf("Size of an double: %zu byte(s)\n", sizeof(double));
 #if defined(__LP64__)
-        printf("Size of a char: %lu byte(s)\n", sizeof(char));
-        printf("Size of an int: %lu byte(s)\n", sizeof(int));
-        printf("Size of an double: %lu byte(s)\n", sizeof(double));
-        printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-        printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-        printf("Size of a float: %lu byte(s)\n", sizeof(float));
+        printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+        printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 #else
-        printf("Size of a char: %lu byte(s)\n", sizeof(char));
-        printf("Size of an int: %lu byte(s)\n", sizeof(int));
-        printf("Size of an double: %lu byte(s)\n", sizeof(double));
-        printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+        printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
         // long long int is not available on 32-bit machines
-        // printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-        printf("Size of a float: %lu byte(s)\n", sizeof(float));
 #endif
+        printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
         return (0);
 
